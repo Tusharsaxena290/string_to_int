fd8b9831db76221cce468a36fbd6c7a9ba888266
Tushar Saxena <tushar.saxena_iot18@gla.ac.in>
2019-08-24 13:30:59 +0530

    dialog box appearing

--- src/user_input.java
+++ src/user_input.java
@@ -1,5 +1,8 @@
+import javax.swing.*;
 public class user_input {
     public static void main(String[] args) {
-
+        string name;
+        name = JOptionPane.showInputDialog("ENTER YOUR NAME");
+        System.out.println(name);
     }
 }
