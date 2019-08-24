355c2fedd67bc0636b043084b49736cf840770cc
Tushar Saxena <tushar.saxena_iot18@gla.ac.in>
2019-08-24 13:32:23 +0530

    enter your name

--- src/user_input.java
+++ src/user_input.java
@@ -3,6 +3,6 @@ public class user_input {
     public static void main(String[] args) {
         string name;
         name = JOptionPane.showInputDialog("ENTER YOUR NAME");
-        System.out.println(name)
+        System.out.println(name);
     }
 }
